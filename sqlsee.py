12a8e96aefb46a37cf3a31d5d95b1c369760d027
G3N3RA710N-L0CU57 <m.jenkins.software@gmail.com>
2022-11-20 14:12:19 -0500

    started iterator class

--- sqlsee.py
+++ sqlsee.py
@@ -4,7 +4,9 @@ import urllib.request
 import gzip
 
 ###### HTTP request ######
+
 class HTTPrequest():
+""" Sends HTTP request and retrieves response. """
 
     def __init__(self, url, header, data=None):
         self.url = url
@@ -15,16 +17,19 @@ class HTTPrequest():
 
 
     def send_request(self):
-        print("Making request")
+    """ Send HTTP request and decompress response if gzip compressed. """
         with urllib.request.urlopen(self.request) as response:
             self.response = response.read()
+            # Check if gzip compressed.
             if self.response[0:2] == b'\x1f\x8b':
                 self.response = gzip.decompress(self.response)
 
     def get_response(self):
+    """ Return HTTP response. """
         return self.response
 
     def _format_header(self, header):
+    """ Format list of headers input from cli and return a dict.  """
         self.header_list = header.split(", ")
         self.header_dict = dict()
         for i in range(len(self.header_list)):
@@ -34,12 +39,49 @@ class HTTPrequest():
 
         return self.header_dict
 
+
+###### Iterator ######
+
+class Iterator(HTTPrequest):
+
+    def __init__(self, url, header, data=None, query):
+        super().__init__(url, header, data)
+        self.query = query
+
+    def iterate_columns(self):
+        for i in range(1, 20):
+            self.send_request()
+
+
+###### Injection base class ######
+
+class BaseInjection(Iterator):
+
+    def __init__(self):
+        pass
+
+
+    def find_columns(self):
+        pass
+
+
+###### MySQL ######
+
+class MySQLunion(BaseInjection):
+
+    def __init__(self):
+        pass
+
+
+
+
 ###### Command line parser ######
 parser = argparse.ArgumentParser()
 
 parser.add_argument("url", help="url of target in format http://x.x.x.x:port/path")
 parser.add_argument("-H", "--header", help="Header values in comma seperated list. See README for formatting. ex. Host: x.x.x.x, Accept: text/html")
-parser.add_argument("-rF", "--request-file", help="request in the form of a .txt file ex. -rF request.txt")
+parser.add_argument("-fE", "--first-esc", help="First escape character used in query. ex. -fE '")
+parser.add_argument("-lE", "--last-esc", help="Last escape character used in query, usually a comment. ex. -- -")
 
 ###### Main function ######
 def main():
