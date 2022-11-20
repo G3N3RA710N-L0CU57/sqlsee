c8c220a94fb3115ac6afa2ba3bd2894495ecb914
G3N3RA710N-L0CU57 <m.jenkins.software@gmail.com>
2022-11-20 11:30:19 -0500

    url header formatting

--- sqlsee.py
+++ sqlsee.py
@@ -1,21 +1,35 @@
 #!/bin/python3
 import argparse
+import urllib
 
 
 ###### HTTP request ######
 class HTTPrequest():
 
-        def __init__(self, url, header):
-                self.url = url
-                self.header = header
+    def __init__(self, url, header, data=None):
+        self.url = url
+        self.header = self._format_header(header)
+        self.data = data
+        #self.request = urllib.request.Request(self.url, self.data, self.header)
 
 
-        def send_request(self):
-                pass
+    def send_request(self):
+        pass
 
-        def get_response(self):
-                pass
+    def get_response(self):
+        pass
 
+    def _format_header(self, header):
+        self.header_list = header.split(", ")
+        self.header_dict = dict()
+        for i in range(len(self.header_list)):
+            self.header_list[i] = self.header_list[i].strip()
+            self.header_seperate = self.header_list[i].split(": ")
+            self.header_dict[self.header_seperate[0]] = self.header_seperate[1]
+
+        print(self.header_dict)
+
+        return self.header_dict
 
 ###### Command line parser ######
 parser = argparse.ArgumentParser()
@@ -26,9 +40,8 @@ parser.add_argument("-rF", "--request-file", help="request in the form of a .txt
 
 ###### Main function ######
 def main():
-        args = parser.parse_args()
-        print(args.url)
-        print(args.header)
+    args = parser.parse_args()
+    url_obj = HTTPrequest(args.url, args.header)
 
 if __name__ == "__main__":
-        main()
+    main()
